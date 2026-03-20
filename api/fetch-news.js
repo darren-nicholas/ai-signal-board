89839018cdbb8b64ec567eed4c8d51991b969b58
darren-nicholas <darren.nicholas1@gmail.com>
2026-03-20 14:20:03 -0700

    Update fetch-news.js

--- api/fetch-news.js
+++ api/fetch-news.js
@@ -238,6 +238,10 @@ async function saveDailyChallenge(challenge) {
 }
 
 export default async function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+  if (req.method === "OPTIONS") return res.status(200).end();
+  
   try {
     console.log("Starting news fetch...");
 
